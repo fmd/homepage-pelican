e0c8966497b0dd86e0d3ca924043233f80c21292
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-06 22:40:45 +0000

    Updated peliconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,10 +17,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS = None
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
